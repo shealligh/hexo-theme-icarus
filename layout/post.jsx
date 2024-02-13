d37421268f193113499c50e7fe1d839259301ac3
shpbyyy <1951696101@qq.com>
2024-02-13 10:35:02 +0800

    [feat] update post.jsx: force display of table of content widget

--- layout/post.jsx
+++ layout/post.jsx
@@ -4,7 +4,7 @@ const Article = require('./common/article');
 module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
-
+        config.toc = true;
         return <Article config={config} page={page} helper={helper} index={false} />;
     }
 };
